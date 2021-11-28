f7f219b7754631b0254879b57ba85011a76b8df6
savelmtr <savelmtr@gmail.com>
2021-11-28 19:28:44 +0300

    setup fix

--- machina_search/__init__.py
+++ machina_search/__init__.py
@@ -1,2 +1,2 @@
-__version__ = 0.1
+__version__ = '0.1'
 default_app_config = 'machina_search.apps.ForumSearchAppConfig'
--- setup.py
+++ setup.py
@@ -3,18 +3,16 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-HERE = Path(__file__).parent
 
-print(HERE)
 setup(
     name='machina_search',
-version=re.search(r"__version__\s*?=\s*?'(.*?)'", open(HERE/'machina_search/__init__.py').read()).group(1),
+version=re.search(r"__version__\s*?=\s*?'([\s\S]*?)'", open('machina_search/__init__.py').read()).group(1),
     description='Postgres search for django-machina.',
-    long_description=open(HERE/'README.md').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/savelmtr/django-machina-search',
     packages=find_packages(include=['machina_search', 'machina_search.*']),
-    install_requires=open(HERE/'requirements.txt').read().split('\n'),
+    install_requires=open('requirements.txt').read().split('\n'),
     include_package_data=True,
     test_suite='tests',
 )
