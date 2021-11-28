334848da279a45f455f3238a7cc8e89a83ef8708
savelmtr <savelmtr@gmail.com>
2021-11-28 20:21:54 +0300

    models fix

--- machina_search/__init__.py
+++ machina_search/__init__.py
@@ -1,2 +1,2 @@
 __version__ = '0.1'
-default_app_config = 'machina_search.apps.ForumSearchAppConfig'
+default_app_config = 'machina_search.apps.PostgresForumSearchAppConfig'
--- machina_search/apps.py
+++ machina_search/apps.py
@@ -11,7 +11,7 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class ForumSearchAppConfig(AppConfig):
+class PostgresForumSearchAppConfig(AppConfig):
     label = 'machina_search'
     name = 'machina_search'
     verbose_name = _('Machina: Forum searches')
