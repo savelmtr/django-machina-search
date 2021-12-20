28ed6ea4005b2e32871f844386c4db9adf41d7dc
savelmtr <savelmtr@gmail.com>
2021-12-21 00:02:03 +0300

    settings tuning

--- machina_search/settings.py
+++ machina_search/settings.py
@@ -2,5 +2,7 @@ from django.conf import settings
 from machina.conf import settings as machina_conf
 
 TOPIC_POSTS_NUMBER_PER_PAGE = getattr(machina_conf, 'TOPIC_POSTS_NUMBER_PER_PAGE', 15)
-SEARCH_ENGINE = getattr(settings, 'SEARCH_ENGINE', 'default')
+SEARCH_ENGINE = 'postgres' \
+    if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql' else \
+    'default'
 SEARCH_LANGUAGE = getattr(settings, 'SEARCH_LANGUAGE', 'english')
