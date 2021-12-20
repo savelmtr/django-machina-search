401d6b5ad153d85075ebe61db951d32d87733ba9
savelmtr <savelmtr@gmail.com>
2021-12-20 22:30:28 +0300

    headline

--- machina_search/managers.py
+++ machina_search/managers.py
@@ -70,7 +70,7 @@ class SearchManager(models.Manager):
         '''
         if settings.SEARCH_ENGINE == 'postgres':
             select_query += f", ts_headline('pg_catalog.{settings.SEARCH_LANGUAGE}'" \
-                            f", p.content , query) as headline"
+                            f", p.content , query, MaxWords=40) as headline"
             search_vector_field = self._get_vector_field(search_topics)
             query = f'''
                 select
