6f27edb156970ecb4e7710bbcf44cfbc6fa4722d
savelmtr <savelmtr@gmail.com>
2021-12-20 22:17:28 +0300

    headline

--- machina_search/templatetags/machina_search_tags.py
+++ machina_search/templatetags/machina_search_tags.py
@@ -6,5 +6,6 @@ register = template.Library()
 @register.filter
 @stringfilter
 def search_header(value):
-    value = value.replace('\n\n', '</p><p>')
+    print(value)
+    value = value.replace('\n', '</p><p>')
     return f"<p>{value}</p>"
