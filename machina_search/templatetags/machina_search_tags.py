338b7d87c58c7b50bb6decf1157d80f448feed61
savelmtr <savelmtr@gmail.com>
2021-12-21 18:06:33 +0300

    makes paging

--- machina_search/templatetags/machina_search_tags.py
+++ machina_search/templatetags/machina_search_tags.py
@@ -1,11 +1,21 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from machina.models.fields import render_func
+import re
 
 
 register = template.Library()
 
 @register.filter
 @stringfilter
-def search_header(value):
+def search_header(value: str) -> str:
     return render_func(value).replace('[mark]', '<mark>').replace('[/mark]', '</mark>')
+
+
+@register.simple_tag(takes_context=True)
+def insert_page_in_url(context: dict, num: int) -> str:
+    url = context['request'].get_full_path()
+    if 'page=' in url:
+    url = re.sub(r'page=\d*', f'page={num}', url)
+    else:
+        url += f'&page={num}'
