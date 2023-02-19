061ea365685e323d8b5059a6ef4aa2a7a6c8a42c
savelmtr <savelmtr@gmail.com>
2023-02-20 01:40:43 +0300

    addes prefilling search index at migration

--- machina_search/migrations/0002_prepare_postgres_search.py
+++ machina_search/migrations/0002_prepare_postgres_search.py
@@ -43,6 +43,21 @@ class Migration(migrations.Migration):
 
                     UPDATE machina_search_postssearchindex
                     SET search_vector_all = NULL, search_vector_subject = NULL;
+
+                    -- FILL THE INDEX
+
+                    INSERT into machina_search_postssearchindex 
+                        (topic, search_vector_all, search_vector_subject)
+                    SELECT
+                        p.id,
+                        setweight(to_tsvector('pg_catalog.{0}', coalesce(p.subject, '')), 'A') ||
+                        setweight(to_tsvector('pg_catalog.{0}', coalesce(p.content, '')), 'B'),
+                        to_tsvector('pg_catalog.{0}', coalesce(p.subject, ''))
+                    FROM forum_conversation_post p
+                    ON CONFLICT ("topic") 
+                    DO UPDATE SET
+                        "search_vector_all" = EXCLUDED.search_vector_all,
+                        "search_vector_subject" = EXCLUDED.search_vector_subject;
                     '''.format(settings.SEARCH_LANGUAGE),
 
                     reverse_sql='''
